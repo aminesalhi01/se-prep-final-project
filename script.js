73a7c1f9a440f3480a74d5bf37c40b3d3deed2ba
Amine Salhi <aminesalhi01@gmail.com>
2023-02-01 19:29:24 +0100

    index2.html and script2.js created

--- script.js
+++ script.js
@@ -10,3 +10,26 @@ helloButton.addEventListener("click", function(){
         helloSection.classList.remove("hide-hello")
     }
 })
+let skillButton = document.querySelector(".click-btn-skills");
+let skillSection = document.querySelector(".skills div");
+
+skillButton.addEventListener("click", function(){
+    if (skillSection.classList.contains("hidden-skills")===false){
+        skillSection.classList.add("hidden-skills");
+    }
+    else{
+        skillSection.classList.remove("hidden-skills")
+    }
+})
+
+let contactButton = document.querySelector(".click-btn-contact");
+let contactSection = document.querySelector(".contact-me div");
+
+contactButton.addEventListener("click", function(){
+    if(contactSection.classList.contains("hidden-contact")=== false){
+        contactSection.classList.add("hidden-contact");
+    }
+    else{
+        contactSection.classList.remove("hidden-contact");
+    }
+})
